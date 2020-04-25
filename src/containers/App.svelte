c1e0fc104c004b60fa28c9438a1c0c462c6c1c4a
Kegovc <octaviovcruz@hotmail.com>
2020-04-25 16:53:26 -0500

    configuracion minima Svelte

---
+++ src/containers/App.svelte
@@ -0,0 +1,15 @@
+
+<style>
+    :global(body){
+        background-color: #676778;
+        color: withe;
+    }
+    .main{
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
+<div class="main">
+    <h1>Hello Pugstagram</h1>
+</div>
