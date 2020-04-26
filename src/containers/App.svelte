c83823202ebc4e2b124ca05d06161e7f2ef4ed47
Kegovc <octaviovcruz@hotmail.com>
2020-04-25 19:56:49 -0500

    publish

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,15 +1,53 @@
+<script>
+  import { onMount } from 'svelte'
+
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+
+
+
+  let data = {}
+
+  let API = "https://us-central1-pugstagram-co.cloudfunctions.net/data"
+
+  onMount(async ()=>{
+
+      console.log("onMount")
+    try {
+      const response = await fetch(`${API}`)
+      data = await response.json()
+      console.log(data)
+      data.user ={
+        nickname: "KEGOVC",
+        name:"Octavio Villavicencio Cruz"
+      }
+    } catch (error) {
+      
+    }
+  })
+
+</script>
 
 <style>
-    :global(body){
-        background-color: #676778;
-        color: withe;
-    }
-    .main{
-        display: flex;
-        justify-content: center;
-    }
+  @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+  :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  :global(h1, h2, h3){
+      margin: 0;
+      padding: 0;
+  }
 </style>
 
-<div class="main">
-    <h1>Hello Pugstagram</h1>
-</div>
+<Header />
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
